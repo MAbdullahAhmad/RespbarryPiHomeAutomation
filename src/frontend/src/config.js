1ab61998850d028771c991dcb8071b02ee495f67
Abdullah Ahmad <theneweratech@gmail.com>
2024-09-18 21:56:10 +0500

    added dynamic ip for frontend

--- src/frontend/src/config.js
+++ src/frontend/src/config.js
@@ -1,2 +1,2 @@
-const host = 'http://127.0.0.1:8000'
-export const make_url = endpoint => host + endpoint;
+const host = window.location.hostname + ':8000';
+export const make_url = endpoint => `http://${host}${endpoint}`;
